cca985e844882b6ed5b3b71f3ef03f1b6534e15b
Ron Dyck <61429024+dyckr23@users.noreply.github.com>
2021-05-25 12:07:42 -0600

    Add SigningKey field to Connector type
    This commit also adds a SetSigningKey method for connectors to receive
    a PEM encoded key as string and set a resultant *rsa.PrivateKey as the
    SigningKey. Also starts building up the AccessToken method.

--- connector/connector.go
+++ connector/connector.go
@@ -9,16 +9,25 @@ package connector
 
 import (
 	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"log"
 	"net/url"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jwt"
 	"github.com/macewan-cs/lti/datastore"
 	"github.com/macewan-cs/lti/datastore/nonpersistent"
 )
+
+// Access Token validity period in minutes.
+const AccessTokenTimeoutMinutes = 60
 
 // Config represents the configuration used in creating a new *Connector. New will accept the zero value of this struct,
 // and in the case of the zero value, the resulting Connector will use nonpersistent storage.
@@ -33,7 +42,8 @@ type Connector struct {
 	cfg         Config
 	LaunchID    string
 	LaunchToken jwt.Token
-	//SigningKeyFunc
+	SigningKey  *rsa.PrivateKey
+	//SigningKeyFunc func([]byte) (*rsa.PrivateKey, error)
 	//Scopes []url.URL
 }
 
@@ -64,11 +74,11 @@ func New(cfg Config, launchID string) *Connector {
 	if connector.cfg.LaunchData == nil {
 		connector.cfg.LaunchData = nonpersistent.DefaultStore
 	}
-	if connector.cfg.AccessTokens == nil {
-		connector.cfg.AccessTokens = nonpersistent.DefaultStore
-	}
 	if connector.cfg.Registrations == nil {
 		connector.cfg.Registrations = nonpersistent.DefaultStore
+	}
+	if connector.cfg.AccessTokens == nil {
+		connector.cfg.AccessTokens = nonpersistent.DefaultStore
 	}
 
 	// New could perhaps return (*Connector, error).
@@ -78,6 +88,27 @@ func New(cfg Config, launchID string) *Connector {
 	}
 
 	return &connector
+}
+
+// SetSigningKey takes a PEM encoded private key and sets a Connector's signing key to the corresponing RSA private key.
+func (c *Connector) SetSigningKey(pemPrivateKey string) error {
+	if len(pemPrivateKey) == 0 {
+		return errors.New("received empty signing key")
+	}
+
+	pemPrivateKeyBytes := []byte(pemPrivateKey)
+	pemBlock, _ := pem.Decode(pemPrivateKeyBytes)
+	if pemBlock == nil {
+		return errors.New("failed to decode PEM key block")
+	}
+	rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(pemBlock.Bytes)
+	if err != nil {
+		return errors.New("failed to parse RSA key")
+	}
+
+	c.SigningKey = rsaPrivateKey
+
+	return nil
 }
 
 // setTokenFromLaunchData populates the Connector's token with stored launch data that is derived from the OIDC id_token
@@ -172,12 +203,31 @@ func (c *Connector) UpgradeAGS() (*AGS, error) {
 	return nil, nil
 }
 
-// AccessToken (*AGS receiver) gets AND sets a scoped bearer token for an AGS call.
-func (a *AGS) AccessToken(scopes []url.URL) error {
+// AccessToken (*NRPS receiver) gets AND sets a scoped bearer token for an NRPS call.
+func (n *NRPS) AccessToken(scopes []url.URL) error {
+	registration, err := n.Target.getRegistration()
+	if err != nil {
+		return err
+	}
+
+	token := jwt.New()
+	token.Set(jwt.IssuerKey, registration.ClientID)
+	token.Set(jwt.AudienceKey, registration.Issuer)
+	token.Set(jwt.ExpirationKey, time.Now().Add(time.Minute*AccessTokenTimeoutMinutes))
+	token.Set(jwt.IssuedAtKey, time.Now())
+	token.Set(`nonce`, uuid.New().String())
+
+	buf, err := json.MarshalIndent(token, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to generate JSON: %s", err)
+	}
+
+	fmt.Printf("%s\n", buf)
+
 	return nil
 }
 
-// AccessToken (*NRPS receiver) gets AND sets a scoped bearer token for an NRPS call.
-func (n *NRPS) AccessToken(scopes []url.URL) error {
+// AccessToken (*AGS receiver) gets AND sets a scoped bearer token for an AGS call.
+func (a *AGS) AccessToken(scopes []url.URL) error {
 	return nil
 }
